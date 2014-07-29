6770c68665930bd179ed4706ba0f995b4f4df92c
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-07-29 18:06:04 +0200

    Add rspec to this plugin

--- init.rb
+++ init.rb
@@ -5,4 +5,5 @@ Redmine::Plugin.register :redmine_watch_project do
   version '0.0.1'
   url 'https://github.com/jbbarth/redmine_watch_project'
   author_url 'jeanbaptiste.barth@gmail.com'
+  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.1'
 end
