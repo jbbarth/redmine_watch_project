eeb626ef486fd3c93a8d1bf22813fdac3bd66f85
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-07-30 13:28:46 +0200

    Automatically add/remove current user from issues when watching a project
    For performance issues, the 'add' part is only performed on open issues,
    but it doesn't matter because we will add an automatic watch on each
    issue save in the next commit

--- app/controllers/watch_project_controller.rb
+++ app/controllers/watch_project_controller.rb
@@ -3,14 +3,31 @@ class WatchProjectController < ApplicationController
   before_filter :require_member
 
   def watch
+    #watch project
     @project.watcher_users << User.current
     @project.save
+    #watch all open issues on project
+    issue_ids = @project.issues.open.map(&:id)
+    already_watched = Watcher.where(:watchable_type => "Issue", :watchable_id => issue_ids).pluck(:id)
+    (issue_ids - already_watched).each do |id|
+      Watcher.create(:watchable_type => "Issue",
+                     :watchable_id   => id,
+                     :user_id        => User.current.id)
+    end
+    #redirect
     redirect_back_or_default project_issues_path(@project)
   end
 
   def unwatch
+    #unwatch project
     @project.watcher_users = @project.watcher_users - [User.current]
     @project.save
+    #unwatch all issues on project
+    Watcher.where(:user_id => User.current.id,
+                  :watchable_type => "Issue",
+                  :watchable_id   => @project.issues.pluck(:id))
+           .delete_all
+    #redirect
     redirect_back_or_default project_issues_path(@project)
   end
 
--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -16,32 +16,54 @@ describe WatchProjectController do
   end
 
   context "authorized" do
+    let(:project) { Project.find(5) }
+
     describe "POST watch" do
       it "adds current user to project's watchers" do
-        project = Project.find(5)
         session[:user_id] = 1
         expect {
           post :watch, :project_id => project.id
         }.to change {
-          Watcher.count
+          Watcher.where(:watchable_type => "Project").count
         }.by 1
         response.should redirect_to(project_issues_path(project))
+      end
+
+      it "watches directly all (open) issues in the project" do
+        session[:user_id] = 1
+        expect {
+          post :watch, :project_id => project.id
+        }.to change {
+          Watcher.where(:watchable_type => "Issue").count
+        }.by project.issues.open.count
       end
     end
 
     describe "DELETE unwatch" do
       it "removes current user from project's watchers" do
-        project = Project.find(5)
         project.watcher_users << User.find(1)
         project.save!
         session[:user_id] = 1
         expect {
           delete :unwatch, :project_id => project.id
         }.to change {
-          Watcher.count
+          Watcher.where(:watchable_type => "Project").count
         }.by -1
         response.should redirect_to(project_issues_path(project))
       end
+
+      it "unwatches directly all issues in the project" do
+        project.issues.each do |issue|
+          Watcher.create(:watchable_type => "Issue", :watchable_id => issue.id, :user_id => 1)
+        end
+        session[:user_id] = 1
+        expect {
+          delete :unwatch, :project_id => project.id
+        }.to change {
+          Watcher.where(:watchable_type => "Issue").count
+        }.by -project.issues.count
+      end
+
     end
   end
 end
