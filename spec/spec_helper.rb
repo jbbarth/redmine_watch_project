5f2f2f2b0617c59cfed78088d67c3012a1b2971a
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-07-30 06:41:06 +0200

    Add moar things to spec_helper so we can run actual specs with fixtures, mocks, etc.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,24 @@
 ENV['RAILS_ENV'] ||= 'test'
 
+#load rails/redmine
 require File.expand_path('../../../../config/environment', __FILE__)
+
+#test gems
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'rspec/mocks'
+require 'rspec/mocks/standalone'
+require 'pry'
+
+#load paths
+$:.<< File.expand_path('../../app/models', __FILE__)
+$:.<< File.expand_path('../../lib', __FILE__)
+
+#rspec base config
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.fixture_path = "#{::Rails.root}/test/fixtures"
+  config.use_transactional_fixtures = true
+end
