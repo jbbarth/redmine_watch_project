4338480a4560252a4c8730e88244cd184661f6be
Vincent Robert <vincent.robert@nanego.com>
2016-01-20 17:55:41 +0100

    New issues cannot have locked watchers - Fix this bug

--- lib/redmine_watch_project/issue_patch.rb
+++ lib/redmine_watch_project/issue_patch.rb
@@ -4,6 +4,7 @@ class Issue
   before_save :add_project_watchers_as_watchers
 
   def add_project_watchers_as_watchers
-    self.watcher_user_ids += self.project.watcher_user_ids
+    project_watchers = User.where(id: self.project.watcher_user_ids).active # exclude locked users
+    self.watcher_user_ids += project_watchers.map(&:id)
   end
 end
