5dacc2b351228619be82a4eae7116b752f99c6da
Vincent Robert <vincent.robert@nanego.com>
2016-07-26 17:06:54 +0200

    Improve compatibility with Redmine 3.3.0 test suite

--- lib/redmine_watch_project/issue_patch.rb
+++ lib/redmine_watch_project/issue_patch.rb
@@ -5,6 +5,6 @@ class Issue
 
   def add_project_watchers_as_watchers
     project_watchers = User.where(id: self.project.watcher_user_ids).active # exclude locked users
-    self.watcher_user_ids += project_watchers.map(&:id)
+    self.watchers |= project_watchers
   end
 end
