8ba090648ed27b534b990c842c8490e6c2edb8f5
Vincent Robert <vincent.robert@nanego.com>
2016-07-26 17:19:58 +0200

    Ensure we don't override current not saved watchers

--- lib/redmine_watch_project/issue_patch.rb
+++ lib/redmine_watch_project/issue_patch.rb
@@ -5,6 +5,6 @@ class Issue
 
   def add_project_watchers_as_watchers
     project_watchers = User.where(id: self.project.watcher_user_ids).active # exclude locked users
-    self.watchers |= project_watchers
+    self.watchers |= Watcher.where(user_id: project_watchers.map(&:id))
   end
 end
