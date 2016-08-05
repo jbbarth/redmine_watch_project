6108544a8a431256c73d57e2b3eb6d3aad42295d
Vincent Robert <vincent.robert@nanego.com>
2016-08-05 09:16:31 +0200

    Revert latest commits to avoid bugs

--- lib/redmine_watch_project/issue_patch.rb
+++ lib/redmine_watch_project/issue_patch.rb
@@ -5,6 +5,6 @@ class Issue
 
   def add_project_watchers_as_watchers
     project_watchers = User.where(id: self.project.watcher_user_ids).active # exclude locked users
-    self.watchers |= Watcher.where(user_id: project_watchers.map(&:id))
+    self.watcher_user_ids += project_watchers.map(&:id)
   end
 end
