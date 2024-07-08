936d620003906facd81cf2c5631014a8c70630f8
Vincent Robert <vincent.robert@nanego.com>
2024-07-08 12:05:49 +0200

    Avoid adding watchers who cannot access the issue

--- lib/redmine_watch_project/issue_patch.rb
+++ lib/redmine_watch_project/issue_patch.rb
@@ -1,15 +1,19 @@
 module RedmineWatchProject
   module IssuePatch
 
-    def self.included(base) # :nodoc:
+    def self.included(base)
       base.class_eval do
         before_save :add_project_watchers_as_watchers
       end
     end
 
     def add_project_watchers_as_watchers
-      project_watchers = User.where(id: self.project.watcher_user_ids).active # exclude locked users
-      self.watcher_user_ids += project_watchers.map(&:id)
+      # exclude locked users and members that can't view all issues
+      project.watcher_users.active.each do |user|
+        if user.roles_for_project(project).any? { |role| role.allowed_to?(:view_issues) && role.issues_visibility != 'own' }
+          watcher_users << user
+        end
+      end
     end
   end
 end
